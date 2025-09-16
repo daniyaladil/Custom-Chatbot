eadf42cbf9c7b11bf20f958e2a26eb6b59937cab
Daniyal Adil <daniyalsardar893@gmail.com>
2025-09-17 02:58:13 +0500

    ..

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
